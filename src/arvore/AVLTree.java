e94c2875fc19f0f7a70b9289f05b3d9fba29dab3
Alisson Bruno <alissonbruno318@gmail.com>
2026-09-17 03:28:38 +0000

    refactor: remove import inutilizado

--- src/arvore/AVLTree.java
+++ src/arvore/AVLTree.java
@@ -2,7 +2,6 @@ package arvore;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 import java.util.function.Consumer;
 
 public class AVLTree<E extends Comparable<E>> {
